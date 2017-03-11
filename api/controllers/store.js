bb40d8c6bbb5ff3d1b693d1da7265eafe201cbc7
Joshua Brummet <jbrumski22@gmail.com>
2017-03-10 19:43:11 -0700

    Delete store.js

--- api/controllers/store.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	"sigad": {
-		"key": "key1",
-		"data": "61a7bf3c372e10c06e63e62656d4b2e5bbc9ff26f70f2448b93d01cc482cb095c9a829809dea541f6c830cb61327fd29ce95dfed77cc7bee71d7a7a813c37ca1"
-	}
-};
